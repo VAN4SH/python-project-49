09b01ac5996f196d4e030095bf999ae0a600e002
root <root@DESKTOP-FILLTED>
2024-04-05 13:40:09 +0300

    add new game

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,44 @@
+import prompt
+from random import randint
+
+
+def is_prime(number):
+    if number < 2:
+        return False
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+def main():
+    print("Welcome to the Brain Games!")
+    name = prompt.string("May I have your name? ")
+    print(f"Hello, {name}!")
+    print('Answer "yes" if given number is prime. Otherwise answer "no".')
+
+    for _ in range(3):
+        num = randint(1, 100)
+        print(f"Question: {num}")
+        user_answer = input("Your answer: ")
+
+        if (user_answer == "yes" and is_prime(num)) or (user_answer == "no" and not is_prime(num)):
+            print("Correct!")
+        else:
+            if is_prime(num):
+                correct_answer = "yes"
+            else:
+                correct_answer = "no"
+            print(f'"{user_answer}" is wrong answer ;(. Correct answer was "{correct_answer}".')
+            print(f"Let's try again, {name}!")
+            return
+
+    print(f"Congratulations, {name}!")
+
+
+
+if __name__ == "__main__":
+    main()
+    
+
+
+
