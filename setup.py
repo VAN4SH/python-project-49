b8fddf6f439db90624918ef921950066f2d4990d
ivan <ivan@dmitriev-o2k.o2k.ru>
2024-07-19 13:11:29 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='first-project-ivaaaandmitriev',
-    version='0.2.0',
-    packages=find_packages(include=['brain_games', 'brain_games.*']),
+    name="first-project-ivaaaandmitriev",
+    version="0.2.0",
+    packages=find_packages(include=["brain_games", "brain_games.*"]),
     entry_points={
-        'console_scripts': [
-                       
-        ],
+        "console_scripts": [],
     },
-    install_requires=[
-        
-    ],
+    install_requires=[],
     include_package_data=True,
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
