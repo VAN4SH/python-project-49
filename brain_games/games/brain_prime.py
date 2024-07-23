099ff39f7c493ca803427dda796d07ee09558b1d
ivan <ivan@dmitriev-o2k.o2k.ru>
2024-07-23 09:58:00 +0300

    fix error

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -1,7 +1,7 @@
 import random
 
 
-TASK = "Answer 'yes' if given number is prime. Otherwise answer 'no'."
+TASK = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime(n):
